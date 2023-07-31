f3b575f4a46707830a120277c70e0fd4500d80a9
aozasa <s-aozasa@cmc-solutions.co.jp>
2023-07-31 22:05:40 +0900

    #12 認証機能の自動テストの作成
    - application controller,viewのテスト

--- backend/src/controllers/applicationController.ts
+++ backend/src/controllers/applicationController.ts
@@ -5,11 +5,14 @@ import { unauthorizedErrorTemplate } from '../views/applicationView';
 export const verifyMiddleware = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const authorization = req.headers.authorization;
   if (authorization != null) {
-    const verifyRes = await Session.verify({ token: authorization.split(' ')[1] });
-    if (verifyRes.success) {
-      const tokenInfo = verifyRes.res;
-      res.locals.user = { id: tokenInfo.sub, role: tokenInfo['custom:role'], name: tokenInfo.name };
-      return next();
+    const regToken = authorization.match(/Bearer\s+(\S+)/);
+    if (regToken != null && regToken[1] != null) {
+      const verifyRes = await Session.verify({ token: regToken[1] });
+      if (verifyRes.success) {
+        const tokenInfo = verifyRes.res;
+        res.locals.user = { id: tokenInfo.sub, role: tokenInfo['custom:role'], name: tokenInfo.name };
+        return next();
+      }
     }
   }
   return res.status(401).send(unauthorizedErrorTemplate());
